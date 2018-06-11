94ba277d4e05887f88d960f6e5c3deb21c1bedc4
Aaron Kendall <Aaron.Kendall@gmail.com>
2018-06-11 17:04:47 -0400

    Added extensions to the WonkaPrd project.

---
+++ WonkaSystem/WonkaPrd/WonkaPrdExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using WonkaRef;
+
+namespace WonkaPrd
+{
+    public static class WonkaPrdExtensions
+    {
+        public static void SetAttribute(this WonkaProduct poTargetProduct, WonkaRefAttr poTargetAttr, string psTargetValue)
+        {
+            if (poTargetProduct.GetProductGroup(poTargetAttr.GroupId).GetRowCount() <= 0)
+                poTargetProduct.GetProductGroup(poTargetAttr.GroupId).AppendRow();
+
+            poTargetProduct.GetProductGroup(poTargetAttr.GroupId)[0][poTargetAttr.AttrId] = psTargetValue;
+        }
+    }
+}
