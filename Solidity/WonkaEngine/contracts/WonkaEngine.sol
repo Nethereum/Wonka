5f917ce5f2f96bcd7e187488e4d94dfe89b89f01
Aaron Kenadll <Aaron.Kendall@gmail.com>
2018-06-22 23:08:11 -0400

    Added function to dynamically retrieve an attribute value from a contract instance via assembly.

--- Solidity/WonkaEngine/contracts/WonkaEngine.sol
+++ Solidity/WonkaEngine/contracts/WonkaEngine.sol
@@ -726,6 +726,44 @@ contract WonkaEngine {
         return string(bytesStringTrimmed);
     }
 
+    function invokeValueRetrieval(address targetContract, address sender, bytes32 methodName, bytes32 attrName) public returns (string strAnswer) {
+
+        bytes32 answer;
+
+        string memory strMethodName = bytes32ToString(methodName);
+
+        string memory functionNameAndParams = strConcat(strMethodName, "(bytes32)");
+
+        bytes4 sig = bytes4(keccak256(abi.encodePacked(functionNameAndParams)));
+
+        assembly {
+            // move pointer to free memory spot
+            let ptr := mload(0x40)
+            // put function sig at memory spot
+            mstore(ptr,sig)
+            // append argument after function sig
+            mstore(add(ptr,0x04), attrName)
+
+            let result := call(
+                15000, // gas limit
+                targetContract,
+                0, // not transfer any ether
+                ptr, // Inputs are stored at location ptr
+                0x24, // Inputs are 36 bytes long
+                ptr,  //Store output over input
+                0x20) //Outputs are 32 bytes long
+            
+            if eq(result, 0) {
+                revert(0, 0)
+            }
+            
+            answer := mload(ptr) // Assign output to answer var
+            mstore(0x40,add(ptr,0x24)) // Set storage pointer to new space
+        }
+
+        strAnswer = bytes32ToString(answer);
+    }
+
     /// @author 
     /// @notice Copied this code from Oraclize - // Copyright (c) 2015-2016 Oraclize srl, Thomas Bertani
     /// @dev This code definitely works
