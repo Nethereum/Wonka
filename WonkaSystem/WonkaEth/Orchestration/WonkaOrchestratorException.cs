463cef177a6bed2511c529775564ad179aa60c99
Aaron Kendall <Aaron.Kendall@gmail.com>
2018-08-14 14:34:29 -0400

    Added exception class.

---
+++ WonkaSystem/WonkaEth/Orchestration/WonkaOrchestratorException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WonkaEth.Validation
+{
+    public class WonkaOrchestratorException : Exception
+    {
+        public readonly WonkaRuleTreeReport RuleTreeReport = null;
+
+        public WonkaOrchestratorException(WonkaRuleTreeReport poReport)
+        {
+            RuleTreeReport = poReport;
+        }
+    }
+}
