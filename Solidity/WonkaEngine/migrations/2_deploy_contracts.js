fc3839f87b5d579188c0e0de9232af80155aa6cb
Aaron Kendall <Aaron.Kendall@gmail.com>
2018-08-29 17:22:53 -0400

    Added lines to scripts so that we are able to deploy the WonkaRegistry.

--- Solidity/WonkaEngine/migrations/2_deploy_contracts.js
+++ Solidity/WonkaEngine/migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 var WeEngine = artifacts.require("./WonkaEngine.sol");
 var TestContract = artifacts.require("./OrchTestContract.sol");
+var WrContract = artifacts.require("./WonkaRegistry.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(TestContract);
   deployer.deploy(WeEngine);
+  deployer.deploy(WrContract);
 };
--- Solidity/WonkaEngine/test/testdeploy.js
+++ Solidity/WonkaEngine/test/testdeploy.js
@@ -1,5 +1,6 @@
 var WonkaEngine = artifacts.require("./WonkaEngine.sol");
 var OrchTestContract = artifacts.require("./OrchTestContract.sol");
+var WonkaRegistry = artifacts.require("./WonkaRegistry.sol");
 
 // NOTE: in mist web3 is already available, so check first if it's available before instantiating
 // var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
@@ -17,21 +18,27 @@ var ASSIGN_RULE       = 5;
 
 contract('WonkaEngine', function(accounts) {
 contract('OrchTestContract', function(accounts) {
+contract('WonkaRegistry', function(accounts) {
   
   it("should be 3 attributes stored in the engine", function() {
     return WonkaEngine.deployed().then(function(wInstance) {
       return OrchTestContract.deployed().then(function(tInstance) {
+        return WonkaRegistry.deployed().then(function(rInstance) {
 
-        console.log("Address of WonkaEngine is (" + wInstance.address + ")");
+          console.log("Address of WonkaEngine is (" + wInstance.address + ")");
 
-        console.log("Address of OrchTestContract is (" + tInstance.address + ")");
+          console.log("Address of OrchTestContract is (" + tInstance.address + ")");
 
-        return wInstance.getNumberOfAttributes.call();
-      }).then(function(balance) {
-        assert.equal(balance.valueOf(), 3, "More or less than 3 attributes populated");
+          console.log("Address of WonkaRegistry is (" + rInstance.address + ")");
+
+          return wInstance.getNumberOfAttributes.call();
+        }).then(function(balance) {
+          assert.equal(balance.valueOf(), 3, "More or less than 3 attributes populated");
+        });
       });
     });
   });
 
+});
 });
 });
