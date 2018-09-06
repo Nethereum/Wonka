e13aa19df79dbdbe6ff2208f9481a0c51b0c37d6
Aaron Kendall <Aaron.Kendall@gmail.com>
2018-09-06 16:32:50 -0400

    Added functionality for Groves into the Registry contract.

--- Solidity/WonkaEngine/contracts/WonkaRegistry.sol
+++ Solidity/WonkaEngine/contracts/WonkaRegistry.sol
@@ -6,6 +6,26 @@ pragma solidity ^0.4.24;
 /// @dev 
 contract WonkaRegistry {
 
+    /// @title Defines a rule grove
+    /// @author Aaron Kendall
+    /// @notice This class will provide information on a Grove (i.e., a collection of RuleTrees)
+    struct WonkaRuleGrove {
+
+        bytes32 ruleGroveId;
+
+        string description;
+
+        bytes32[] ruleTreeMembers;
+
+        mapping(bytes32 => uint) memberPositions;
+
+        address owner;
+
+        uint creationEpochTime;
+        
+        bool isValue;
+    }
+
     /// @title Defines a ruletree index
     /// @author Aaron Kendall
     /// @notice This class will provide information on any RuleTree in the 'tree-verse' (which contract owns it, who owns it, the cost associated with it, etc.)
@@ -38,12 +58,15 @@ contract WonkaRegistry {
         bool isValue;
     }
 
+    string constant CONST_DEFAULT_VALUE = "Default";
+
+    // The cache of all rule groves
+    mapping(bytes32 => WonkaRuleGrove) private ruleGroves;
+    bytes32[] private ruleGrovesEnum;
+
     // The cache of all rule trees 
     mapping(bytes32 => WonkaRuleTreeIndex) private ruleTrees;
     bytes32[] private ruleTreesEnum;
-
-    // The grove names, their members, and the order in which they should be applied to a record
-    mapping(bytes32 => mapping(bytes32 => uint)) private ruleGroves;
 
     /// @dev Constructor for the RuleTree registry
     /// @author Aaron Kendall
@@ -52,12 +75,35 @@ contract WonkaRegistry {
         // NOTE: Initialize members here
     }
 
-    /// @dev This method will add a new index to the registry
+    /// @dev This method will add a new grove to the registry
+    /// @author Aaron Kendall
+    /// @notice 
+    function addRuleGrove(bytes32 groveId, string desc, address groveOwner, uint createTime) public {
+
+        require(groveId != "");
+
+        require(ruleGroves[groveId].isValue != true);
+
+        ruleGroves[groveId] = WonkaRuleGrove({
+            ruleGroveId: groveId,
+            description: desc,
+            ruleTreeMembers: new bytes32[](0),
+            owner: groveOwner,
+            creationEpochTime: createTime,
+            isValue: true
+        });
+
+        ruleGrovesEnum.push(groveId);
+    }
+
+    /// @dev This method will add a new tree index to the registry
     /// @author Aaron Kendall
     /// @notice 
     function addRuleTreeIndex(address ruler, bytes32 rsId, string desc, bytes32 ruleTreeGrpId, uint grpIdx, address host, uint minCost, uint maxCost, address[] associates, bytes32[] attributes, bytes32[] ops, uint createTime) public {
 
         // require(msg.sender == rulesMaster);
+
+        require(rsId != "");
 
         require(ruleTrees[rsId].isValue != true);
 
@@ -80,11 +126,35 @@ contract WonkaRegistry {
 
         if (ruleTreeGrpId != "") {
 
+            if (ruleGroves[ruleTreeGrpId].isValue != true)
+                addRuleGrove(ruleTreeGrpId, CONST_DEFAULT_VALUE, ruler, createTime);
+
             ruleTrees[rsId].ruleTreeGroveIds.push(ruleTreeGrpId);
 
-            if (grpIdx < ruleTreesEnum.length) 
-                ruleGroves[ruleTreeGrpId][rsId] = grpIdx;
+            ruleGroves[ruleTreeGrpId].ruleTreeMembers.push(rsId);
+
+            if ((grpIdx > 0) && (grpIdx <= ruleGroves[ruleTreeGrpId].ruleTreeMembers.length)) {
+                ruleGroves[ruleTreeGrpId].memberPositions[rsId] = grpIdx;
+            }
         }
+    }
+
+    /// @dev This method will add a RuleTree to an existing Grove
+    /// @author Aaron Kendall
+    /// @notice 
+    function addRuleTreeToGrove(bytes32 groveId, bytes32 treeId) public {
+
+        // require(msg.sender == rulesMaster);
+
+        require(ruleTrees[treeId].isValue == true);
+
+        require(ruleGroves[groveId].isValue != true);
+
+        require(ruleGroves[groveId].memberPositions[treeId] == 0);
+
+        ruleGroves[groveId].ruleTreeMembers.push(treeId);
+
+        ruleGroves[groveId].memberPositions[treeId] = (ruleGroves[groveId].ruleTreeMembers.length + 1);
     }
 
     /// @dev This method will return an index from the registry
@@ -102,50 +172,50 @@ contract WonkaRegistry {
     /// @dev This method will return all rule trees that belong to a specific group, in the order that they should be applied to a record
     /// @author Aaron Kendall
     /// @notice 
-    function getGroupMembers(bytes32 rsGroupId) public view returns (bytes32[]) {
+    function getGroupMembers(bytes32 groveId) public view returns (bytes32[]) {
 
         // require(msg.sender == rulesMaster);
 
-        require(ruleTreesEnum.length > 0);
+        require(ruleGroves[groveId].isValue == true);
 
         uint orderIdx = 0;
 
-        bytes32[] memory groupMembers = new bytes32[](ruleTreesEnum.length);
+        bytes32[] memory groupMembers = new bytes32[](ruleGroves[groveId].ruleTreeMembers.length);
 
-        for (uint i = 0; i < ruleTreesEnum.length; ++i) {
+        for (uint i = 0; i < ruleGroves[groveId].ruleTreeMembers.length; ++i) {
 
-            bytes32 tmpRsId = ruleTreesEnum[i];
+            bytes32 tmpRsId = ruleGroves[groveId].ruleTreeMembers[i];
  
-            orderIdx = ruleGroves[rsGroupId][tmpRsId];
+            orderIdx = ruleGroves[groveId].memberPositions[tmpRsId];
 
-            groupMembers[orderIdx] = tmpRsId;
-                
+            if ((orderIdx > 0) && (orderIdx <= ruleGroves[groveId].ruleTreeMembers.length))
+                groupMembers[orderIdx-1] = tmpRsId;
         }
 
         return groupMembers;
+        // return ruleGroves[groveId].ruleTreeMembers;
     }
 
     /// @dev This method will return the ordered position of the RuleTree 'rsId' within the group 'rsGroupId'
     /// @author Aaron Kendall
     /// @notice 
-    function getGroupOrderPosition(bytes32 rsGroupId, bytes32 rsId) public view returns (uint) {
+    function getGroupOrderPosition(bytes32 groveId, bytes32 rsId) public view returns (uint) {
 
         // require(msg.sender == rulesMaster);
 
-        require(ruleTreesEnum.length > 0);
+        require(ruleGroves[groveId].isValue != true);
 
         uint orderIdx = 999999;
 
-        if (rsGroupId != "") {
+        if (groveId != "") {
 
             if (rsId != "") {
-
-                orderIdx = ruleGroves[rsGroupId][rsId];
+                orderIdx = ruleGroves[groveId].memberPositions[rsId];
             }
         }
 
         return orderIdx;
-    }
+    }    
 
     /// @dev This method will indicate whether or not the RuleTree has been added to the registry
     /// @author Aaron Kendall
@@ -153,12 +223,12 @@ contract WonkaRegistry {
     function isRuleTreeRegistered(bytes32 rsId) public view returns (bool) {
 
         return (ruleTrees[rsId].isValue == true);
-    }    
+    }
 
     /// @dev This method will reorder the members of a rule grove
     /// @author Aaron Kendall
     /// @notice 
-    function resetGroupOrder(bytes32 rsGroupId, bytes32[] rsIdList, uint[] orderList) public {
+    function resetGroupOrder(bytes32 groveId, bytes32[] rsIdList, uint[] orderList) public {
 
         // require(msg.sender == rulesMaster);
 
@@ -168,10 +238,25 @@ contract WonkaRegistry {
 
         require(rsIdList.length == orderList.length);
 
+        require(ruleGroves[groveId].ruleTreeMembers.length == rsIdList.length);
+
         uint idx = 0;
 
+        uint grpIdx = 0;
+
+        bytes32 tmpId = "";
+
         for (idx = 0; idx < rsIdList.length; ++idx) {
-            ruleGroves[rsGroupId][rsIdList[idx]] = orderList[idx];
+
+            tmpId = rsIdList[idx];
+
+            ruleGroves[groveId].ruleTreeMembers[idx] = tmpId;
+
+            grpIdx = ruleGroves[groveId].memberPositions[tmpId];
+
+            if ((grpIdx > 0) && (grpIdx <= ruleGroves[groveId].ruleTreeMembers.length)) {
+                ruleGroves[groveId].memberPositions[tmpId] = grpIdx;
+            }
         }
     }
 
