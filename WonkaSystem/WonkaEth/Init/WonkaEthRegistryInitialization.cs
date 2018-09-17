af801c1a2d29855f56e71b679f60e49902896307
Aaron Kendall <Aaron.Kendall@gmail.com>
2018-09-17 17:52:25 -0400

    Added code to support Registry initialization being stored in a configuration file.

---
+++ WonkaSystem/WonkaEth/Init/WonkaEthRegistryInitialization.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WonkaEth.Init
+{
+    /// <summary>
+    /// 
+    /// This class represents the information needed to communicate with the Registry.
+    /// 
+    /// </summary>
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public class WonkaEthRegistryInitialization
+    {
+        public WonkaEthRegistryInitialization()
+        {
+            BlockchainRegistry = new WonkaEthSource();
+        }
+
+        public void RetrieveEmbeddedResources(System.Reflection.Assembly poTargetAssembly)
+        {
+            if (this.BlockchainRegistry != null)
+            {
+                if (String.IsNullOrEmpty(BlockchainRegistry.ContractABI) && !String.IsNullOrEmpty(BlockchainRegistry.ContractABIEmbeddedResource))
+                {
+                    using (var AbiReader = new System.IO.StreamReader(poTargetAssembly.GetManifestResourceStream(BlockchainRegistry.ContractABIEmbeddedResource)))
+                    {
+                        BlockchainRegistry.ContractABI = AbiReader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        public WonkaEthSource BlockchainRegistry { get; set; }
+    }
+
+}
