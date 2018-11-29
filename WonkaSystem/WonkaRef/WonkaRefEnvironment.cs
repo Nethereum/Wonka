fe9329d036fd3f17f473d0e1b6e61872e1581e91
Aaron Kendall <Aaron.Kendall@gmail.com>
2018-11-29 15:01:11 -0500

    Set the AttrKey collection to be ignored upon serialization, since that collection is a subset built from the Attr collection during construction.

--- WonkaSystem/WonkaRef/WonkaRefEnvironment.cs
+++ WonkaSystem/WonkaRef/WonkaRefEnvironment.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Xml.Serialization;
 
 namespace WonkaRef
 {
@@ -296,7 +297,8 @@ namespace WonkaRef
         #region Standard Metadata Cache (Minimum Set)
 
         public List<WonkaRefAttr>             AttrCache { get; }
-        
+
+        [XmlIgnore]
         public List<WonkaRefAttr>             AttrKeys { get; }
 
         private Dictionary<int, WonkaRefAttr> AttrMap { get; }
