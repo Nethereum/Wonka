7debcd8b66f9139c987a3009e8f89ff9cc038089
Aaron Kendall <Aaron.Kendall@gmail.com>
2018-12-04 11:14:43 -0500

    Implemented the interface.

--- WonkaSystem/WonkaBre/Import/IRuleTreeRetrievable.cs
+++ WonkaSystem/WonkaBre/Import/IRuleTreeRetrievable.cs
@@ -1,7 +1,22 @@
 ﻿using System;
+
+using WonkaRef;
+
 namespace WonkaBre.Import
 {
+    /// <summary>
+    /// 
+    /// This interface will be required when instantiating a rules engine from
+    /// a third-party source (like a BizTalk configuration file).
+    ///     
+    /// </summary>
     public interface IRuleTreeRetrievable
     {
+        IMetadataRetrievable GetMetadata();
+
+        string GetWonkaRulesXml();
+
+        WonkaBreRulesEngine CreateRulesEngine();
     }
+
 }
