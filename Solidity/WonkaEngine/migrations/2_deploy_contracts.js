09d327a77a98d58b00abb3643bebcf0a4037c501
Aaron Kendall <Aaron.Kendall@gmail.com>
2018-12-19 07:35:58 -0500

    Altered script so that it would also deploy and link the WonkaSerializer library contract.

--- Solidity/WonkaEngine/migrations/2_deploy_contracts.js
+++ Solidity/WonkaEngine/migrations/2_deploy_contracts.js
@@ -1,9 +1,12 @@
 var WeEngine = artifacts.require("./WonkaEngine.sol");
 var TestContract = artifacts.require("./OrchTestContract.sol");
 var WrContract = artifacts.require("./WonkaRegistry.sol");
+var WsContract = artifacts.require("./WonkaSerializer.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(TestContract);
-  deployer.deploy(WeEngine);
   deployer.deploy(WrContract);
+  deployer.deploy(WsContract);
+  deployer.link(WsContract, WeEngine);
+  deployer.deploy(WeEngine);
 };
