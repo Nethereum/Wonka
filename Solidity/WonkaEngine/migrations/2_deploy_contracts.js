8478fd1fd28716fd44be1b3ad2a3c3685bc391a9
Aaron Kendall <Aaron.Kendall@gmail.com>
2018-12-28 07:59:12 -0500

    Abandoned idea of a Serializer contract since it's easier for a camel to pass through the eye of an electron than for the blockchain to serialize a complex data structure (stack too deep, bloated gas costs when it comes to deployments, etc.).

--- Solidity/WonkaEngine/migrations/2_deploy_contracts.js
+++ Solidity/WonkaEngine/migrations/2_deploy_contracts.js
@@ -1,12 +1,9 @@
 var WeEngine = artifacts.require("./WonkaEngine.sol");
 var TestContract = artifacts.require("./OrchTestContract.sol");
 var WrContract = artifacts.require("./WonkaRegistry.sol");
-var WsContract = artifacts.require("./WonkaSerializer.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(TestContract);
-  deployer.deploy(WrContract);
-  deployer.deploy(WsContract);
-  deployer.link(WsContract, WeEngine);
   deployer.deploy(WeEngine);
+  deployer.deploy(WrContract);
 };
