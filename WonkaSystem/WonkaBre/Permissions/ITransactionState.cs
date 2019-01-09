b00b7c030040f1e1b82fae27372f0ee5d76217ac
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-01-09 17:10:32 -0500

    Initial revision.

---
+++ WonkaSystem/WonkaBre/Permissions/ITransactionState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WonkaBre.Permissions
+{
+    public interface ITransactionState
+    {
+        void AddOwner(string psNewOwner, int pnWeight);
+
+        void AddConfirmation(string psOwner);
+
+        void ClearPendingTransaction();
+
+        bool HasConfirmed(string psOwner);
+
+        bool IsOwner(string psOwner);
+
+        bool IsTransactionConfirmed();
+
+        void RemoveOwner(string psNewOwner);
+
+        bool RevokeAllConfirmations();
+
+        bool RevokeConfirmation(string psOwner);
+
+        void SetMinRequirement(int pnMinReq);
+    }
+}
