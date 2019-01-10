26655043dcb7dd0e03b67731408b26fdbbfb430f
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-01-10 14:16:04 -0500

    Initial revision.

---
+++ WonkaSystem/WonkaBre/Permissions/WonkaBrePermissionsException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WonkaBre.Permissions
+{
+    /// <summary>
+    /// 
+    /// This exception should be used when encountering a permissions issue with using
+    /// the Business Rules Engine.
+    /// 
+    /// </summary>
+    class WonkaBrePermissionsException : Exception
+    {
+        public WonkaBrePermissionsException(string psErrorMessage) : base(psErrorMessage)
+        {
+        }
+    }
+}
