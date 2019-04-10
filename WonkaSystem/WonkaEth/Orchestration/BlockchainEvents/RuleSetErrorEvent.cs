c349082f8aae43d827d4e30578609a7e0ae1322f
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-04-10 18:04:06 -0400

    Added initial revision of RuleSetError class.

---
+++ WonkaSystem/WonkaEth/Orchestration/BlockchainEvents/RuleSetErrorEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Nethereum.ABI.FunctionEncoding.Attributes;
+
+namespace WonkaEth.Orchestration.BlockchainEvents
+{
+    [Event("RuleSetError")]
+    public class RuleSetErrorEvent
+    {
+        [Parameter("address", "ruler", 1, true)]
+        public string TreeOwner { get; set; }
+
+        [Parameter("bytes32", "ruleSetId", 2, true)]
+        public string RuleSetId { get; set; }
+
+        [Parameter("bool", "severeFailure", 3, false)]
+        public bool SevereFailure { get; set; }
+    }
+}
