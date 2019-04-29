1a6662fdd5db363fcc685a50363f3fc15406cf2a
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-04-29 15:23:54 -0400

    Added/altered functions of interface.

--- Solidity/WonkaEngine/contracts/SchemaGeneratorInterface.sol
+++ Solidity/WonkaEngine/contracts/SchemaGeneratorInterface.sol
@@ -9,5 +9,7 @@ interface SchemaGeneratorInterface
     /**
      ** Expecting ABI types like "uint256", "bytes32", "bool", and "string"
      **/
-    function addSchemaCol(bytes32 abiName, bytes32 abiType) external returns (bool);
+    function addSchemaCol(bytes32 abiName, bytes32 abiType, uint maxLength, uint minValue, uint maxValue) external returns (bool);
+	
+	function addFunction(bytes32 abiName, bytes32[] abiParamNames, bytes32[] abiParamTypes, bytes32[] abiReturnTypes) external returns (bool);
 }
