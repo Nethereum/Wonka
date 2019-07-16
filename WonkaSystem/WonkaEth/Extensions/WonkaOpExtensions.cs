b714b62c03962986a2df74deeb14f948fe93a7d7
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-07-16 12:31:24 -0400

    Initial revision.

---
+++ WonkaSystem/WonkaEth/Extensions/WonkaOpExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Nethereum.Web3.Accounts;
+
+using WonkaBre;
+using WonkaBre.RuleTree;
+
+namespace WonkaEth.Extensions
+{
+    /// <summary>
+    /// 
+    /// This extensions class provides the functionality for Nethereum-related operations, so that it can be invoked
+    /// from within the Wonka rules engine
+    /// 
+    /// </summary>
+    public static class WonkaOpExtensions
+    {
+        private static Dictionary<WonkaBreRulesEngine, Nethereum.Web3.Web3> EngineWeb3Accounts = new Dictionary<WonkaBreRulesEngine, Nethereum.Web3.Web3>();
+
+        /// <summary>
+        /// 
+        /// This method will register the default set of standard operations (especially Nethereum-related ones) that can be 
+        /// invoked from within the Wonka rules engine
+        /// 
+        /// <param name="poEngine">The target instance of an engine</param>
+        /// <returns>None</returns>
+        /// </summary>
+        public static void SetDefaultStdOps(this WonkaBreRulesEngine poEngine, string psPassword, string psWeb3HttpUrl = null)
+        {
+            var account = new Account(psPassword);
+
+            Nethereum.Web3.Web3 web3 = null;
+            if (!String.IsNullOrEmpty(psWeb3HttpUrl))
+                web3 = new Nethereum.Web3.Web3(account, psWeb3HttpUrl);
+            else
+                web3 = new Nethereum.Web3.Web3(account);
+
+            EngineWeb3Accounts[poEngine] = web3;
+
+            poEngine.StdOpMap[STD_OP_TYPE.STD_OP_BLOCKN_NUM] = GetCurrentBlockNum;
+        }
+
+        #region Default Op Functions
+
+        private static string GetCurrentBlockNum(WonkaBreRulesEngine poEngine, string psUnusedVal)
+        {
+            string sCurrBlockNum = "";
+
+            if (EngineWeb3Accounts.ContainsKey(poEngine))
+            {
+                Nethereum.Web3.Web3 EngineWeb3 = EngineWeb3Accounts[poEngine];
+
+                sCurrBlockNum = EngineWeb3.Eth.Blocks.GetBlockNumber.SendRequestAsync().Result.HexValue.ToString();
+            }
+
+            return sCurrBlockNum;
+        }
+
+        #endregion
+
+    }
+}
