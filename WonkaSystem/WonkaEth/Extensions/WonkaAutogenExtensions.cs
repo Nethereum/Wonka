7a688746f8036da38b0f010f2ad21d29dcdaa8a5
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-08-02 13:50:47 -0400

    Added a deploy method that will allow the caller to specify the gas amount.

--- WonkaSystem/WonkaEth/Extensions/WonkaAutogenExtensions.cs
+++ WonkaSystem/WonkaEth/Extensions/WonkaAutogenExtensions.cs
@@ -50,5 +50,40 @@ namespace WonkaEth.Extensions
 
             return contractAddress;
         }
+
+        /// <summary>
+        /// 
+        /// This method will use Nethereum to deploy a Wonka contract to the chain.
+        /// 
+        /// <param name="poDeployMsg">The deployment message with the bytecodes that can create the target contract</param>
+        /// <param name="psWonkaAbi">The ABI of the target contract</param>
+        /// <param name="psSenderAddress">The ABI of the target contract</param>
+        /// <param name="pnGas">The amount of gas supplied for the deployment of the contract</param>
+        /// <param name="psWeb3HttpUrl">The client node to which we will deploy the contract</param>
+        /// <returns>The address of the new instance of the target contract</returns>
+        /// </summary>
+        public static string DeployContract(this ContractDeploymentMessage poDeployMsg, Web3 poWeb3, string psWonkaAbi, string psSenderAddress, Nethereum.Hex.HexTypes.HexBigInteger pnGas, string psWeb3HttpUrl = "")
+        {
+            var transactionHash =
+                poWeb3.Eth.DeployContract.SendRequestAsync(psWonkaAbi, poDeployMsg.ByteCode, psSenderAddress, pnGas).Result;
+
+            // NOTE: Should we start/stop mining by default?
+            // var mineResult = poWeb3.Miner.Start.SendRequestAsync(6).Result;
+
+            var receipt = poWeb3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transactionHash).Result;
+
+            while (receipt == null)
+            {
+                Thread.Sleep(5000);
+                receipt = poWeb3.Eth.Transactions.GetTransactionReceipt.SendRequestAsync(transactionHash).Result;
+            }
+
+            // NOTE: Should we start/stop mining by default?
+            // mineResult = poWeb3.Miner.Stop.SendRequestAsync().Result;
+
+            var contractAddress = receipt.ContractAddress;
+
+            return contractAddress;
+        }
     }
 }
