51b9745f44bfa4bd4f0d1bd537415b2478aee89b
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-08-02 13:53:13 -0400

    Added class that will contain all data needed to deploy Wonka contracts to the chain (and perhaps serialize engine data to them).

---
+++ WonkaSystem/WonkaEth/Init/WonkaEthEngineInitialization.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WonkaBre;
+
+namespace WonkaEth.Init
+{
+    public class WonkaEthEngineInitialization
+    {
+        public WonkaEthEngineInitialization()
+        {
+            RulesEngine = null;
+            Web3HttpUrl = "";
+
+            EthSenderAddress = EthPassword = EthRuleTreeOwnerAddress = "";
+
+            RulesEngineContractAddress = RulesEngineABI      = "";
+            RegistryContractAddress    = RegistryContractABI = "";
+
+            UsingTestContract   = true;
+            TestContractAddress = TestContractABI = "";
+
+            UsingTrxStateContract   = false;
+            TrxStateContractAddress = null;
+        }
+
+        public WonkaBreRulesEngine RulesEngine { get; set; }
+
+        public string Web3HttpUrl { get; set; }
+
+        public string EthSenderAddress { get; set; }
+
+        public string EthPassword { get; set; }
+
+        public string EthRuleTreeOwnerAddress { get; set; }
+
+        public string RulesEngineContractAddress { get; set; }
+
+        public string RulesEngineABI { get; set; }
+
+        public string RegistryContractAddress { get; set; }
+
+        public string RegistryContractABI { get; set; }
+
+        public bool UsingTestContract { get; set; }
+
+        public string TestContractAddress { get; set; }
+
+        public string TestContractABI { get; set; }
+
+        public bool UsingTrxStateContract { get; set; }
+
+        public string TrxStateContractAddress { get; set; }
+
+    }
+}
