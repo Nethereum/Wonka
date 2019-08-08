1b121d77ff1388096e3c0cccb328a9dd96e21911
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-08-08 15:35:02 -0400

    Added extension method GetAttributeValue().

--- WonkaSystem/WonkaPrd/WonkaPrdExtensions.cs
+++ WonkaSystem/WonkaPrd/WonkaPrdExtensions.cs
@@ -6,12 +6,29 @@ namespace WonkaPrd
 {
     public static class WonkaPrdExtensions
     {
-        public static void SetAttribute(this WonkaProduct poTargetProduct, WonkaRefAttr poTargetAttr, string psTargetValue)
-        {
-            if (poTargetProduct.GetProductGroup(poTargetAttr.GroupId).GetRowCount() <= 0)
-                poTargetProduct.GetProductGroup(poTargetAttr.GroupId).AppendRow();
+		public static string GetAttributeValue(this WonkaProduct poTargetProduct, WonkaRefAttr poTargetAttr)
+		{
+			string sAttrValue = "";
 
-            poTargetProduct.GetProductGroup(poTargetAttr.GroupId)[0][poTargetAttr.AttrId] = psTargetValue;
-        }
+			if (poTargetProduct.HasProductGroup(poTargetAttr.GroupId))
+			{
+				if (poTargetProduct.GetProductGroup(poTargetAttr.GroupId)[0].ContainsKey(poTargetAttr.AttrId))
+					sAttrValue = poTargetProduct.GetProductGroup(poTargetAttr.GroupId)[0][poTargetAttr.AttrId];
+			}
+				
+			return sAttrValue;
+		}
+
+		public static bool SetAttribute(this WonkaProduct poTargetProduct, WonkaRefAttr poTargetAttr, string psTargetValue)
+        {
+			bool bSuccess = true;
+
+			if (poTargetProduct.GetProductGroup(poTargetAttr.GroupId).GetRowCount() <= 0)
+				poTargetProduct.GetProductGroup(poTargetAttr.GroupId).AppendRow();
+
+			poTargetProduct.GetProductGroup(poTargetAttr.GroupId)[0][poTargetAttr.AttrId] = psTargetValue;
+
+			return bSuccess;
+		}
     }
 }
