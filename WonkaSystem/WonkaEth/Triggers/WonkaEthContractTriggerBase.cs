ee994f494b4e81e1d83adcb3bad712f674035df0
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-09-16 16:21:29 -0400

    Added WonkaEthContractTriggerBase and used it as the base for WonkaEthEIP20TransferTrigger.

---
+++ WonkaSystem/WonkaEth/Triggers/WonkaEthContractTriggerBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+using Nethereum.Contracts.ContractHandlers;
+using Nethereum.Web3;
+
+namespace WonkaEth.Triggers
+{
+    public class WonkaEthContractTriggerBase
+    {
+        protected Web3                    moWeb3;
+        protected ContractHandler         moContract;
+        protected CancellationTokenSource moCancelToken;
+
+        public WonkaEthContractTriggerBase(Web3 poWeb3, CancellationTokenSource poCancelToken = null)
+		{
+			moWeb3 = poWeb3;
+
+            moContract    = null;
+            moCancelToken = poCancelToken;
+        }
+
+		public WonkaEthContractTriggerBase(Web3 poWeb3, string psContractAddress, CancellationTokenSource poCancelToken = null)
+		{
+			moWeb3 = poWeb3;
+
+            moContract    = moWeb3.Eth.GetContractHandler(psContractAddress);
+            moCancelToken = poCancelToken;
+        }
+
+    }
+}
--- WonkaSystem/WonkaEth/Triggers/WonkaEthEIP20TransferTrigger.cs
+++ WonkaSystem/WonkaEth/Triggers/WonkaEthEIP20TransferTrigger.cs
@@ -1,53 +1,67 @@
 using System;
+using System.Threading;
 using System.Numerics;
 
-using Nethereum.Web3;
+using Nethereum.Contracts.ContractHandlers;
 using Nethereum.StandardTokenEIP20;
 using Nethereum.StandardTokenEIP20.ContractDefinition;
+using Nethereum.Web3;
 
 using WonkaBre;
 using WonkaBre.Triggers;
 
 namespace WonkaEth.Triggers
 {
-	public class WonkaEthEIP20TransferTrigger : ISuccessTrigger
+	public class WonkaEthEIP20TransferTrigger : WonkaEthContractTriggerBase, ISuccessTrigger
 	{
-		private long       mnTotalSupply;
-		private BigInteger mnTransferAmt;
+        protected long       mnTotalSupply;
+        protected BigInteger mnTransferAmt;
 
-		private string msTokenName;
-		private string msTokenSymbol;
-		private string msReceiverAddress;
+        protected string msTokenName;
+        protected string msTokenSymbol;
+        protected string msReceiverAddress;
 
-		private Web3                 moWeb3;
-		private EIP20Deployment      moEIP20Contract;
 		private StandardTokenService moEIP20Service;
 
-		public WonkaEthEIP20TransferTrigger(Web3 poWeb3, long pnTotalSupply, string psTokenName, string psTokenSymbol, string psReceiverAddress, long pnTransferAmount)
+        public WonkaEthEIP20TransferTrigger(Web3 poWeb3, long pnTotalSupply, string psTokenName, string psTokenSymbol, string psReceiverAddress, long pnTransferAmount, CancellationTokenSource poCancelToken = null) 
+            : base(poWeb3, poCancelToken)
 		{
-			mnTotalSupply = pnTotalSupply;
+            mnTotalSupply = pnTotalSupply;
 			mnTransferAmt = pnTransferAmount;
 
 			msTokenName       = psTokenName;
 			msTokenSymbol     = psTokenSymbol;
 			msReceiverAddress = psReceiverAddress;
 
-			moWeb3 = poWeb3;
+            var deploymentContract = new EIP20Deployment()
+            {
+                InitialAmount = new System.Numerics.BigInteger(pnTotalSupply),
+                TokenName     = psTokenName,
+                TokenSymbol   = psTokenSymbol
+            };
 
-			var deploymentContract = new EIP20Deployment()
-			{
-				InitialAmount = new System.Numerics.BigInteger(pnTotalSupply),
-				TokenName     = psTokenName,
-				TokenSymbol   = psTokenSymbol
-			};
+            moEIP20Service = StandardTokenService.DeployContractAndGetServiceAsync(poWeb3, deploymentContract).Result;
+            moContract     = moEIP20Service.ContractHandler;
+        }
 
-			moEIP20Service = StandardTokenService.DeployContractAndGetServiceAsync(poWeb3, deploymentContract).Result;
-		}
+        public WonkaEthEIP20TransferTrigger(Web3 poWeb3, EIP20Deployment moDeployData, string psContractAddress, string psReceiverAddress, long pnTransferAmount, CancellationTokenSource poCancelToken = null)
+            : base(poWeb3, psContractAddress, poCancelToken)
+        {
+            mnTotalSupply = Convert.ToInt64(moDeployData.InitialAmount);
+			mnTransferAmt = pnTransferAmount;
+
+			msTokenName       = moDeployData.TokenName;
+			msTokenSymbol     = moDeployData.TokenSymbol;
+			msReceiverAddress = psReceiverAddress;
+
+            moEIP20Service = new StandardTokenService(poWeb3, psContractAddress);
+            moContract     = moEIP20Service.ContractHandler;
+        }
 
 		public void Execute()
 		{
-			var transferReceipt =
-				moEIP20Service.TransferRequestAndWaitForReceiptAsync(msReceiverAddress, mnTransferAmt).Result;
+            var transferReceipt =
+                moEIP20Service.TransferRequestAndWaitForReceiptAsync(msReceiverAddress, mnTransferAmt, moCancelToken).Result;
 		}
 	}
 }
