698b66a443eb0a5700f405d415670fb365711747
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-12-02 17:05:21 -0500

    Added class WonkaBizGrove.

---
+++ WonkaSystem/WonkaBre/WonkaBizGrove.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wonka.BizRulesEngine
+{
+    public class WonkaBizGrove
+    {
+        public WonkaBizGrove()
+        {
+            GroveId   = 0;
+            GroveDesc = String.Empty;
+            RuleTrees = new List<WonkaBizRulesEngine>();
+        }
+
+        public WonkaBizGrove(int pnGroveId, string psGroveDesc)
+        {
+            GroveId   = pnGroveId;
+            GroveDesc = psGroveDesc;
+            RuleTrees = new List<WonkaBizRulesEngine>();
+        }
+
+        public int GroveId { get; }
+
+        public string GroveDesc { get; }
+
+        public List<WonkaBizRulesEngine> RuleTrees { get; set; }
+    }
+}
