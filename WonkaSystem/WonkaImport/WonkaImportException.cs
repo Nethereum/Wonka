8c07dd9664b542dad9048aee2873b44ef22f1b74
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-12-12 22:18:39 -0500

    Created basic exception for WonkaImport library.

---
+++ WonkaSystem/WonkaImport/WonkaImportException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wonka.Import
+{
+    public class WonkaImportException : Exception
+    {
+        public WonkaImportException(string psErrorMessage) : base(psErrorMessage)
+        {
+            // NOTE: We might add more later
+        }
+    }
+}
+
+
