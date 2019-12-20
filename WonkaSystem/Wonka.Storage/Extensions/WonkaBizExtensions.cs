037489d2b513de2a3161d51d1b6749c38d874035
Aaron Kendall <Aaron.Kendall@gmail.com>
2019-12-20 18:04:55 -0500

    Added code to generate instances of WonkaBizQueryDomainRule.

---
+++ WonkaSystem/Wonka.Storage/Extensions/WonkaBizExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Wonka.BizRulesEngine;
+using Wonka.BizRulesEngine.RuleTree;
+using Wonka.BizRulesEngine.RuleTree.RuleTypes;
+using Wonka.Eth.Extensions;
+using Wonka.MetaData;
+using Wonka.Product;
+
+using Wonka.Storage.Rules;
+
+namespace Wonka.Storage.Extensions
+{
+    public static class WonkaBizExtensions
+    {
+		/// <summary>
+		/// 
+		/// This method will create a custom operator rule that will determine whether an attribute's value falls within a domain defined by the
+        /// results of a query on a database.
+        /// 
+        /// NOTE: UNDER CONSTRUCTION
+		/// 
+		/// </summary>
+		public static CustomOperatorRule BuildSqlDomainQueryRule(this WonkaBizSource poSource, int pnRuleID, int pnTargetAttrId, string psCustomOpName)
+        {
+            return new WonkaBizQueryDomainRule(pnRuleID, pnTargetAttrId, psCustomOpName, poSource);
+        }
+    }
+}
---
+++ WonkaSystem/Wonka.Storage/Rules/WonkaBizQueryDomainRule.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Wonka.BizRulesEngine;
+using Wonka.BizRulesEngine.RuleTree;
+using Wonka.BizRulesEngine.RuleTree.RuleTypes;
+
+namespace Wonka.Storage.Rules
+{
+    public class WonkaBizQueryDomainRule: CustomOperatorRule
+    {
+        public WonkaBizQueryDomainRule(int pnRuleID, int pnTargetAttrId, string psCustomOpName, WonkaBizSource poCustomOpSource) : 
+            base(pnRuleID, TARGET_RECORD.TRID_NEW_RECORD, pnTargetAttrId, psCustomOpName, null, poCustomOpSource)
+        {
+            this.CustomOpDelegate = ExecuteQuery;
+        }
+
+        /// <summary>
+        /// 
+        /// This method will do a data lookup of the value by calling a query on a table in the database.
+        ///
+        /// NOTE: UNDER CONSTRUCTION
+        ///
+        /// </summary>
+        public string ExecuteQuery(string psArg1, string psArg2, string psArg3, string psArg4)
+        {
+            string sQueryValue = "";
+
+            string sConnString = "Data Source=" + this.CustomOpContractSource.SqlServer +
+                                 ";Initial Catalog=" + this.CustomOpContractSource.SqlDatabase +
+                                 ";User ID=" + this.CustomOpContractSource.SqlUsername +
+                                 ";Password=" + this.CustomOpContractSource.SqlPassword;
+
+            // NOTE: Use parameters in prepared statement with query here?
+            string sSqlQuery = this.CustomOpContractSource.SqlQueryOrProcedure;
+
+            using (SqlConnection DbConn = new SqlConnection(sConnString))
+            {
+                try
+                {
+                    DbConn.Open();
+
+                    using (SqlCommand QueryCmd = new SqlCommand(sSqlQuery, DbConn))
+                    {
+                        using (SqlDataReader dataReader = QueryCmd.ExecuteReader())
+                        {
+                            if (dataReader.Read())
+                            {
+                                // NOTE: Additional work needed
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // NOTE: Do something here?
+                }
+            }            
+
+            return sQueryValue;
+        }
+    }
+}
