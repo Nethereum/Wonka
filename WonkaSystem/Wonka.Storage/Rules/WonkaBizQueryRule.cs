77cdf0be09745ee4860670502b1e2c763c8a68e2
Aaron Kendall <Aaron.Kendall@gmail.com>
2020-01-02 17:19:21 -0500

    Added more ideas to the implentation.

--- WonkaSystem/Wonka.Storage/Rules/WonkaBizQueryRule.cs
+++ WonkaSystem/Wonka.Storage/Rules/WonkaBizQueryRule.cs
@@ -13,16 +13,33 @@ namespace Wonka.Storage.Rules
 {
     public class WonkaBizQueryRule: CustomOperatorRule
     {
-        public WonkaBizQueryRule(int pnRuleID, WonkaBizSource poCustomOpSource) :
+        public WonkaBizQueryRule(int pnRuleID, WonkaBizSource poCustomOpSource, bool pbCacheDomain = false) :
             base(pnRuleID, TARGET_RECORD.TRID_NEW_RECORD, 0, null, null, poCustomOpSource)
         {
-            this.CustomOpDelegate = ExecuteQuery;
+            Init(pbCacheDomain);
         }
 
-        public WonkaBizQueryRule(int pnRuleID, int pnTargetAttrId, string psCustomOpName, WonkaBizSource poCustomOpSource) : 
+        public WonkaBizQueryRule(int pnRuleID, int pnTargetAttrId, string psCustomOpName, WonkaBizSource poCustomOpSource, bool pbCacheDomain = false) : 
             base(pnRuleID, TARGET_RECORD.TRID_NEW_RECORD, pnTargetAttrId, psCustomOpName, null, poCustomOpSource)
         {
-            this.CustomOpDelegate = ExecuteQuery;
+            Init(pbCacheDomain);
+        }
+
+        /// <summary>
+        /// 
+        /// This method will do a data lookup of the value by looking for the value in the cached domain.
+        ///
+        /// </summary>
+        public string ExecuteCacheCheck(string psArg1, string psArg2, string psArg3, string psArg4)
+        {
+            string sResultValue = "";
+
+            if (IsDomainQuery && !String.IsNullOrEmpty(psArg1) && CachedDomain.Contains(psArg1))
+            {
+                sResultValue = psArg1;
+            }
+
+            return sResultValue;
         }
 
         /// <summary>
@@ -36,20 +53,15 @@ namespace Wonka.Storage.Rules
         {
             string sResultValue = "";
 
-            string sConnString = "Data Source=" + this.CustomOpContractSource.SqlServer +
-                                 ";Initial Catalog=" + this.CustomOpContractSource.SqlDatabase +
-                                 ";User ID=" + this.CustomOpContractSource.SqlUsername +
-                                 ";Password=" + this.CustomOpContractSource.SqlPassword;
-
-            // NOTE: Use parameters in prepared statement with query here?
-            string sSqlQuery = 
-                String.Format(this.CustomOpContractSource.SqlQueryOrProcedure, psArg1, psArg2, psArg3, psArg4);
-
-            using (SqlConnection DbConn = new SqlConnection(sConnString))
+            try
             {
-                try
+                using (SqlConnection DbConn = GetConnection())
                 {
                     DbConn.Open();
+
+                    // NOTE: Use parameters in prepared statement with query here?
+                    string sSqlQuery =
+                        String.Format(this.CustomOpContractSource.SqlQueryOrProcedure, psArg1, psArg2, psArg3, psArg4);
 
                     using (SqlCommand QueryCmd = new SqlCommand(sSqlQuery, DbConn))
                     {
@@ -64,18 +76,18 @@ namespace Wonka.Storage.Rules
                         }
                     }
                 }
-                catch (Exception ex)
-                {
-                    // NOTE: Do something here?
-                }
-            }            
+            }
+            catch (Exception ex)
+            {
+                // NOTE: Do something here?
+            }
 
             return sResultValue;
         }
 
         /// <summary>
         /// 
-        /// This method will assist with serializing the data to the chain
+        /// This method will assist with retrieving the entire domain
         ///
         /// NOTE: UNDER CONSTRUCTION
         ///
@@ -86,15 +98,76 @@ namespace Wonka.Storage.Rules
 
             if (IsDomainQuery)
             {
-                // NOTE: Populate the set here
+                try
+                {
+                    using (SqlConnection DbConn = GetConnection())
+                    {
+                        DbConn.Open();
+
+                        // NOTE: Use parameters in prepared statement with query here?
+                        string sSqlQuery =
+                            String.Format(this.CustomOpContractSource.SqlQueryOrProcedure);
+
+                        using (SqlCommand QueryCmd = new SqlCommand(sSqlQuery, DbConn))
+                        {
+                            using (SqlDataReader dataReader = QueryCmd.ExecuteReader())
+                            {
+                                if (dataReader.Read())
+                                {
+                                    // NOTE: For the time being, it will return the first value of the first column
+                                    if (!dataReader.IsDBNull(0))
+                                    {
+                                        string sResultValue = dataReader[0].ToString();
+                                        CachedDomain.Add(sResultValue);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // NOTE: Do something here?
+                }
             }
 
             return DomainSet;
         }
 
+        #region SUPPORT METHODS
+
+        private SqlConnection GetConnection()
+        {
+            string sConnString = "Data Source=" + this.CustomOpContractSource.SqlServer +
+                     ";Initial Catalog=" + this.CustomOpContractSource.SqlDatabase +
+                     ";User ID=" + this.CustomOpContractSource.SqlUsername +
+                     ";Password=" + this.CustomOpContractSource.SqlPassword;
+
+            return new SqlConnection(sConnString);
+        }
+
+        protected void Init(bool pbCacheDomain)
+        {
+            this.CachedDomain = new HashSet<string>();
+
+            if (pbCacheDomain)
+            {
+                RetrieveDomainSet();
+                this.CustomOpDelegate = ExecuteCacheCheck;
+            }
+            else
+            {
+                this.CustomOpDelegate = ExecuteQuery;
+            }
+        }
+
+        #endregion
+
         #region PROPERTIES
 
         public bool IsDomainQuery { get; set; }
+
+        private HashSet<string> CachedDomain { get; set; }
 
         #endregion
     }
