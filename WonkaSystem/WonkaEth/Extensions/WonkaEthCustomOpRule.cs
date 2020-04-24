f924fb62da87c89daade3df321a1223ebb0a8f76
Aaron Kendall <Aaron.Kendall@gmail.com>
2020-04-23 23:40:17 -0400

    Added initial code for classes/extensions that will support easily adding Nethereum-based rules to a Wonka RuleTree.

---
+++ WonkaSystem/WonkaEth/Extensions/WonkaEthCustomOpRule.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Globalization;
+using System.Numerics;
+
+using Nethereum.StandardTokenEIP20.ContractDefinition;
+using Nethereum.Web3;
+
+using Wonka.BizRulesEngine;
+using Wonka.BizRulesEngine.Readers;
+using Wonka.BizRulesEngine.RuleTree;
+using Wonka.BizRulesEngine.RuleTree.RuleTypes;
+
+namespace Wonka.Eth.Extensions
+{
+	public class WonkaEthCustomOpRule : CustomOperatorRule
+	{
+		#region PROPERTIES
+
+		public Web3 OwnerWeb3 { get; set; }
+
+		public string PrimaryContractAddress { get; set; }
+
+		#endregion
+
+		public WonkaEthCustomOpRule() : base()
+        {}
+
+        public WonkaEthCustomOpRule(int pnRuleID) : base(pnRuleID)
+        {}
+
+        public WonkaEthCustomOpRule(int pnRuleID,
+                                  TARGET_RECORD peTargetRecord,
+                                  int pnTargetAttrId,
+                                  string psCustomOpName,
+                                  WonkaBizRulesXmlReader.ExecuteCustomOperator poCustomOpDelegate,
+                                  WonkaBizSource poCustomOpSource)
+            : base(pnRuleID, peTargetRecord, pnTargetAttrId, psCustomOpName, poCustomOpDelegate, poCustomOpSource)
+        {}
+
+		public Nethereum.StandardTokenEIP20.StandardTokenService GetERC20TokenService()
+		{
+			return new Nethereum.StandardTokenEIP20.StandardTokenService(OwnerWeb3, PrimaryContractAddress);
+		}
+
+		public string InvokeERC20GetBalance(string psOwner = "", string psDummyVal1 = "", string psDummyVal2 = "", string psDummyVal3 = "")
+		{
+			var tokenService = GetERC20TokenService();
+
+			var balance = tokenService.BalanceOfQueryAsync(new BalanceOfFunction() { Owner = psOwner }).Result;
+
+			return balance.ToString();
+		}
+
+		public string InvokeERC20GetTotalSupply(string psDummyVal1 = "", string psDummyVal2 = "", string psDummyVal3 = "", string psDummyVal4 = "")
+		{
+			var tokenService = GetERC20TokenService();
+
+			var totalSupply = tokenService.TotalSupplyQueryAsync().Result;
+
+			return totalSupply.ToString();
+
+		}
+
+		public string InvokeERC20Transfer(string psToAccount, string psTransferAmt, string psDummyVal1 = "", string psDummyVal2 = "")
+		{
+			var tokenService = GetERC20TokenService();
+
+			BigInteger nAmtToSend = BigInteger.Parse(psTransferAmt, NumberStyles.AllowHexSpecifier);
+
+			var trxReceipt = tokenService.TransferRequestAndWaitForReceiptAsync(new TransferFunction() { To = psToAccount, AmountToSend = nAmtToSend }).Result;
+
+			return trxReceipt.TransactionHash;
+
+		}
+	}
+}
---
+++ WonkaSystem/WonkaEth/Extensions/WonkaEthCustomRuleERC20Extensions.cs
@@ -0,0 +1,84 @@
+using System;
+using Nethereum.Web3;
+
+using Wonka.BizRulesEngine;
+using Wonka.BizRulesEngine.RuleTree;
+using Wonka.MetaData;
+
+namespace Wonka.Eth.Extensions
+{
+	public static class WonkaEthCustomRuleERC20Extensions
+	{
+		private static int mnRuleCounter = 100000;
+
+		public static void AddNethereumERC20GetBalanceRule(this WonkaBizRuleSet poRuleSet,
+			                                                WonkaRefEnvironment poRefEnv, 
+															               Web3 poWeb3,
+															       WonkaRefAttr poTargetAttr,
+															             string psAddRuleDesc,
+															             string psTargetOwner,
+																	     string psERC20ContractAddress)
+		{
+			WonkaBizRule NewRule = null;
+
+			WonkaBizSource DummySource =
+				new WonkaBizSource("ERC20", psERC20ContractAddress, "", "", "", "", "", null);
+
+			WonkaEthCustomOpRule CustomOpRule =
+				new WonkaEthCustomOpRule(mnRuleCounter++,
+									     TARGET_RECORD.TRID_NEW_RECORD,
+										 poTargetAttr.AttrId,
+									     "GET_ERC20_BALANCE",
+									     null,
+									     DummySource);
+
+			CustomOpRule.OwnerWeb3              = poWeb3;
+			CustomOpRule.PrimaryContractAddress = psERC20ContractAddress;
+			CustomOpRule.CustomOpDelegate       = CustomOpRule.InvokeERC20GetBalance;
+
+			CustomOpRule.AddDomainValue(psTargetOwner, true, TARGET_RECORD.TRID_NONE);
+
+			if (!String.IsNullOrEmpty(psAddRuleDesc))
+				NewRule.DescRuleId = psAddRuleDesc;
+
+			poRuleSet.AddRule(NewRule);
+		}
+
+		public static void AddNethereumERC20TransferRule(this WonkaBizRuleSet poRuleSet,
+														  WonkaRefEnvironment poRefEnv,
+														   			     Web3 poWeb3,
+																 WonkaRefAttr poTargetAttr,
+														       		   string psAddRuleDesc,
+																	   string psTargetOwner,
+																	   string psERC20ContractAddress,
+																	   string psTransferReceiverAddress,
+																	   string psAmountToSend)
+		{
+			WonkaBizRule NewRule = null;
+
+			WonkaBizSource DummySource =
+				new WonkaBizSource("ERC20", psERC20ContractAddress, "", "", "", "", "", null);
+
+			WonkaEthCustomOpRule CustomOpRule =
+				new WonkaEthCustomOpRule(mnRuleCounter++,
+										 TARGET_RECORD.TRID_NEW_RECORD,
+										 poTargetAttr.AttrId,
+										 "ERC20_TRANSFER",
+										 null,
+										 DummySource);
+
+			CustomOpRule.OwnerWeb3              = poWeb3;
+			CustomOpRule.PrimaryContractAddress = psERC20ContractAddress;
+			CustomOpRule.CustomOpDelegate       = CustomOpRule.InvokeERC20Transfer;
+
+			CustomOpRule.AddDomainValue(psTransferReceiverAddress, true, TARGET_RECORD.TRID_NONE);
+			CustomOpRule.AddDomainValue(psAmountToSend,            true, TARGET_RECORD.TRID_NONE);
+
+			if (!String.IsNullOrEmpty(psAddRuleDesc))
+				NewRule.DescRuleId = psAddRuleDesc;
+
+			poRuleSet.AddRule(NewRule);
+		}
+	}
+}
+
