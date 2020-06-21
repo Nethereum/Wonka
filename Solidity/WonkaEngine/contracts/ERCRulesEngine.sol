d739ead0046fc735fb57574bdc67c54ddceed2b9
Aaron Kendall <Aaron.Kendall@gmail.com>
2020-06-20 22:40:48 -0400

    Proposed EIP interface of Wonka.

---
+++ Solidity/WonkaEngine/contracts/ERCRulesEngine.sol
@@ -0,0 +1,110 @@
+pragma solidity ^0.6.0;
+
+/**
+    @title ERC-#### Rules Engine Standard
+    @dev See https://eips.ethereum.org/EIPS/eip-####
+ */
+ interface ERCRulesEngine {
+
+    /**
+        @dev Should emit when a RuleTree is invoked.
+        The `ruler` is the ID and owner of the RuleTree being invoked.  It is also likely msg.sender.
+    */
+    event CallRuleTree(
+        address indexed ruler
+    );
+
+    /**
+        @dev Should emit when a RuleSet is invoked.
+        The `ruler` is the ID and owner of the RuleTree in which the RuleSet is stored.  It is also likely msg.sender.
+        The 'ruleSetId' is the ID of the RuleSet being invoked.
+    */
+    event CallRuleSet(
+        address indexed ruler,
+        bytes32 indexed tmpRuleSetId
+    );
+
+    /**
+        @dev Should emit when a Rule is invoked.
+        The `ruler` is the ID and owner of the RuleTree in which the RuleSet is stored.  It is also likely msg.sender.
+        The 'ruleSetId' is the ID of the RuleSet being invoked.
+        The 'ruleId' is the ID of the Rule being invoked.
+        The 'ruleType' is the type of the rule being invoked.        
+    */
+    event CallRule(
+        address indexed ruler,
+        bytes32 indexed ruleSetId,
+        bytes32 indexed ruleId,
+        uint ruleType
+    );
+
+    /**
+        @dev Should emit when a RuleSet fails.
+        The `ruler` is the ID and owner of the RuleTree in which the RuleSet is stored.  It is also likely msg.sender.
+        The 'ruleSetId' is the ID of the RuleSet being invoked.
+        The 'severeFailure' is the indicator of whether or not the RuleSet is a leaf with a 'severe' error flag.
+    */
+    event RuleSetError (
+        address indexed ruler,
+        bytes32 indexed ruleSetId,
+        bool severeFailure
+    );	
+
+    /**
+        @notice Adds a new Attribute to the data domain.
+        @dev Caller should be the deployer/owner of the rules engine contract.  An Attribute value can be an optional alternative if it's not a string or numeric.
+        @param _attrName    Name/ID of the Attribute
+        @param _maxLen      Maximum length of the Attribute (if it is a string)
+        @param _maxNumVal   Maximum numeric value of the Attribute (if it is numeric)
+        @param _defaultVal  The default value for the Attribute
+        @param _isString    Indicator of whether or not the Attribute is a string
+        @param _isNumeric   Indicator of whether or not the Attribute is numeric
+    */    
+    function addAttribute(bytes32 _attrName, uint _maxLen, uint _maxNumVal, string calldata _defaultVal, bool _isString, bool _isNumeric) external;
+
+    /**
+        @notice Adds a new RuleTree.
+        @param _owner          Owner/ID of the RuleTree
+        @param _ruleTreeName   Name of the RuleTree
+        @param _desc           Verbose description of the RuleTree's purpose
+    */
+    function addRuleTree(address _owner, bytes32 _ruleTreeName, string calldata _desc) external;
+
+    /**
+        @notice Adds a new RuleSet onto the hierarchy of a RuleTree.
+        @dev RuleSets can have child RuleSets, but they will only be called if the parent's Rules execute to create boolean 'true'.
+        @param _owner           Owner/ID of the RuleTree
+        @param _ruleSetName     ID/Name of the RuleSet
+        @param _desc            Verbose description of the RuleSet
+        @param _parentRSName    ID/Name of the parent RuleSet, to which this will be added as a child
+        @param _severalFailFlag Indicator of whether or not the RuleSet's execution (as failure) will result in a failure of the RuleTree.  (This flag only applies to leaves in the RuleTree.)
+        @param _useAndOp        Indicator of whether or not the rules in the RuleSet will execute with 'AND' between them.  (Otherwise, it will be 'OR'.)
+        @param _failQuickFlag   Indicator of whether or not the RuleSet's execution (as failure) should immediately stop the RuleTree.
+    */    
+    function addRuleSet(address _owner, bytes32 _ruleSetName, string calldata _desc, bytes32 _parentRSName, bool _severalFailFlag, bool _useAndOp, bool _failQuickFlag) external;
+
+    /**
+        @notice Adds a new Rule into a RuleSet.
+        @dev Rule types can be implemented as any type of action (greater than, less than, etc.)
+        @param _owner           Owner/ID of the RuleTree
+        @param _ruleSetName     ID/Name of the RuleSet to which the Rule will be added
+        @param _ruleName        ID/Name of the Rule being added
+        @param _attrName        ID/Name of the Attribute upon which the Rule is invoked
+        @param _ruleType        ID of the type of Rule
+        @param _rightHandValue  The registered value to be used by the Rule when performing its action upon the Attribute
+        @param _notFlag         Indicator of whether or not the NOT operator should be performed on this Rule.
+    */    
+    function addRule(address _owner, bytes32 _ruleSetName, bytes32 _ruleName, bytes32 _attrName, uint _ruleType, string calldata _rightHandValue, bool _notFlag) external;
+
+    /**
+        @notice Executes a RuleTree.
+        @param _owner           Owner/ID of the RuleTree
+    */
+    function executeRuleTree(address _owner) external returns (bool);
+
+    /**
+        @notice Removes a RuleTree.
+        @param _owner           Owner/ID of the RuleTree
+    */
+    function removeRuleTree(address _owner) external returns (bool);    
+}
