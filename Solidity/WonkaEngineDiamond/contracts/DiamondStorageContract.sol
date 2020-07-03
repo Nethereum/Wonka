697f4914abfe9785675b9a04f41091c865a53128
Aaron Kendall <Aaron.Kendall@gmail.com>
2020-07-02 22:07:43 -0400

    Started Diamond version of the Solidity Wonka engine.

---
+++ Solidity/WonkaEngineDiamond/contracts/DiamondStorageContract.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.8;
+
+contract DiamondStorageContract {
+
+    struct DiamondStorage {
+        
+        // owner of the contract
+        address contractOwner;
+
+        // maps function selectors to the facets that execute the functions.
+        // and maps the selectors to the slot in the selectorSlots array.
+        // and maps the selectors to the position in the slot.
+        // func selector => address facet, uint64 slotsIndex, uint64 slotIndex
+        mapping(bytes4 => bytes32) facets;
+
+        // array of slots of function selectors.
+        // each slot holds 8 function selectors.
+        mapping(uint => bytes32) selectorSlots;  
+
+        // uint128 numSelectorsInSlot, uint128 selectorSlotsLength
+        // selectorSlotsLength is the number of 32-byte slots in selectorSlots.
+        // selectorSlotLength is the number of selectors in the last slot of
+        // selectorSlots.
+        uint selectorSlotsLength;
+
+        // Used to query if a contract implements an interface.
+        // Used to implement ERC-165.
+        mapping(bytes4 => bool) supportedInterfaces;
+    }
+    
+
+    function diamondStorage() internal pure returns(DiamondStorage storage ds) {
+        // ds_slot = keccak256("diamond.standard.diamond.storage");
+        assembly { ds_slot := 0xc8fcad8db84d3cc18b4c41d551ea0ee66dd599cde068d998e57d5e09332c131c }
+    }
+}
---
+++ Solidity/WonkaEngineDiamond/truffle-config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  compilers: {
+    solc: {
+      version: "0.6.8",
+    }
+  },
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+      , gas: 8388608
+      // , gas: 10000000
+    },
+    nethereum: {
+      host: "testchain.nethereum.com",
+      port: 8545,
+      network_id: "*" // match any network
+      , gas: 8388608
+      //, websockets: true
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/YOUR_API_KEY")
+      },
+      network_id: 3,
+      gas: 4000000      //make sure this gas allocation isn't over 4M, which is the max
+    }
+  }
+  , solc: {
+    optimizer: {
+      enabled: true,
+      runs: 180
+    }
+  }	
+};
