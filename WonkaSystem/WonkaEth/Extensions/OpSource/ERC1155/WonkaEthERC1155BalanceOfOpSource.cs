6e970ff998f4ad3f057faa59837ba23d4dfca980
Aaron Kendall <Aaron.Kendall@gmail.com>
2020-08-24 17:16:34 -0400

    Renamed class to correct ERC 1155 standard.

--- WonkaSystem/WonkaEth/Extensions/OpSource/ERC1155/WonkaEthERC1155BalanceOfOpSource.cs
+++ WonkaSystem/WonkaEth/Extensions/OpSource/ERC1155/WonkaEthERC1155BalanceOfOpSource.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace Wonka.Eth.Extensions.OpSource.ERC1155
 {
-	public class WonkaEthERC1150BalanceOfOpSource : WonkaEthERC1155OpSource
+	public class WonkaEthERC1155BalanceOfOpSource : WonkaEthERC1155OpSource
 	{
-		public WonkaEthERC1150BalanceOfOpSource(string psSourceId, string psSenderAddr, string psPwd, string psContractAddr, string psCustomOpMethodName, string psWeb3Url = "") :
+		public WonkaEthERC1155BalanceOfOpSource(string psSourceId, string psSenderAddr, string psPwd, string psContractAddr, string psCustomOpMethodName, string psWeb3Url = "") :
 			base(psSourceId, psSenderAddr, psPwd, psContractAddr, psCustomOpMethodName, psWeb3Url)
 		{
 			this.CustomOpDelegate = this.InvokeERC1155BalanceOf;
