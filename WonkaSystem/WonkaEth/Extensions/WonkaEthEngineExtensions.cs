2df74d5f21f2c80e84a49d4a771a3d88852b6599
Aaron Kendall <Aaron.Kendall@gmail.com>
2020-09-25 15:14:54 -0400

    * Renamed class WonkaEngineDeployment to WonkaEngineDeploymentClassic. * Refactored current deployment code for Wonka contracts, so that it now sits in the method DeployContractsClassicAsync().

--- WonkaSystem/WonkaEth/Extensions/WonkaEthEngineExtensions.cs
+++ WonkaSystem/WonkaEth/Extensions/WonkaEthEngineExtensions.cs
@@ -123,6 +123,66 @@ namespace Wonka.Eth.Extensions
 
         /// <summary>
         /// 
+        /// This method will deploy all contracts needed to run Wonka on-chain, using all the data provided.
+        /// 
+        /// <returns>None</returns>
+        /// </summary>
+        public static async Task<bool> DeployContractsClassicAsync(this WonkaEthEngineInitialization poEngineInitData)
+        {
+			bool bResult = true;
+
+            var EngineProps = poEngineInitData.Engine;
+
+            if (EngineProps == null)
+                throw new Exception("ERROR!  No engine properties provided.");
+
+            if ((EngineProps.RulesEngine == null) && !String.IsNullOrEmpty(EngineProps.RulesMarkupXml))
+            {
+                var account = new Account(poEngineInitData.EthPassword);
+
+                Nethereum.Web3.Web3 web3 = null;
+                if (!String.IsNullOrEmpty(poEngineInitData.Web3HttpUrl))
+                    web3 = new Nethereum.Web3.Web3(account, poEngineInitData.Web3HttpUrl);
+                else
+                    web3 = new Nethereum.Web3.Web3(account);
+
+                if (String.IsNullOrEmpty(poEngineInitData.RulesEngineContractAddress))
+                {
+                    var EngineDeployment = new Autogen.WonkaEngine.WonkaEngineDeploymentClassic();
+
+                    HexBigInteger nDeployGas = new HexBigInteger(CONST_DEPLOY_ENGINE_CONTRACT_GAS_COST);
+
+                    poEngineInitData.RulesEngineContractAddress =
+                        await EngineDeployment.DeployContractAsync(web3, poEngineInitData.RulesEngineABI, poEngineInitData.EthSenderAddress, nDeployGas, poEngineInitData.Web3HttpUrl).ConfigureAwait(false);
+                }
+
+                if (String.IsNullOrEmpty(poEngineInitData.RegistryContractAddress))
+                {
+                    var RegistryDeployment = new Autogen.WonkaRegistry.WonkaRegistryDeployment();
+
+                    HexBigInteger nDeployGas = new HexBigInteger(CONST_DEPLOY_DEFAULT_CONTRACT_GAS_COST);
+
+                    poEngineInitData.RegistryContractAddress =
+                        await RegistryDeployment.DeployContractAsync(web3, poEngineInitData.RegistryContractABI, poEngineInitData.EthSenderAddress, nDeployGas, poEngineInitData.Web3HttpUrl).ConfigureAwait(false);
+                }
+
+                if (String.IsNullOrEmpty(poEngineInitData.StorageContractAddress))
+                {
+                    var TestContractDeployment = new Autogen.WonkaTestContract.WonkaTestContractDeployment();
+
+                    HexBigInteger nDeployGas = new HexBigInteger(CONST_DEPLOY_DEFAULT_CONTRACT_GAS_COST);
+
+                    poEngineInitData.StorageContractAddress =
+                        await TestContractDeployment.DeployContractAsync(web3, poEngineInitData.StorageContractABI, poEngineInitData.EthSenderAddress, nDeployGas, poEngineInitData.Web3HttpUrl).ConfigureAwait(false);
+                }
+            }
+
+            return bResult;
+        }
+
+
+        /// <summary>
+        /// 
         /// This method will initialize an instance of the Wonka.Net engine, using all the data provided.
         /// 
         /// <returns>None</returns>
@@ -145,72 +205,41 @@ namespace Wonka.Eth.Extensions
 				// Using the metadata source, we create an instance of a defined data domain
 				WonkaRefEnvironment WonkaRefEnv = WonkaRefEnvironment.CreateInstance(false, EngineProps.MetadataSource);
 
-				var account = new Account(poEngineInitData.EthPassword);
+                bool bDeploySuccess = poEngineInitData.DeployContractsClassicAsync().Result;
 
-				Nethereum.Web3.Web3 web3 = null;
-				if (!String.IsNullOrEmpty(poEngineInitData.Web3HttpUrl))
-					web3 = new Nethereum.Web3.Web3(account, poEngineInitData.Web3HttpUrl);
-				else
-					web3 = new Nethereum.Web3.Web3(account);
+                if (bDeploySuccess)
+                {
+                    if ((EngineProps.SourceMap == null) || (EngineProps.SourceMap.Count == 0))
+                    {
+                        EngineProps.SourceMap = new Dictionary<string, WonkaBizSource>();
 
-				if (String.IsNullOrEmpty(poEngineInitData.RulesEngineContractAddress))
-				{
-					var EngineDeployment = new Autogen.WonkaEngine.WonkaEngineDeployment();
+                        // Here a mapping is created, where each Attribute points to a specific contract and its "accessor" methods
+                        // - the class that contains this information (contract, accessors, etc.) is of the WonkaBreSource type
+                        foreach (WonkaRefAttr TempAttr in WonkaRefEnv.AttrCache)
+                        {
+                            WonkaBizSource TempSource =
+                                new WonkaBizSource(poEngineInitData.StorageDefaultSourceId,
+                                                   poEngineInitData.EthSenderAddress,
+                                                   poEngineInitData.EthPassword,
+                                                   poEngineInitData.StorageContractAddress,
+                                                   poEngineInitData.StorageContractABI,
+                                                   poEngineInitData.StorageGetterMethod,
+                                                   poEngineInitData.StorageSetterMethod,
+                                                   EngineProps.DotNetRetrieveMethod);
 
-					HexBigInteger nDeployGas = new HexBigInteger(CONST_DEPLOY_ENGINE_CONTRACT_GAS_COST);
+                            EngineProps.SourceMap[TempAttr.AttrName] = TempSource;
+                        }
+                    }
 
-					poEngineInitData.RulesEngineContractAddress =
-						EngineDeployment.DeployContract(web3, poEngineInitData.RulesEngineABI, poEngineInitData.EthSenderAddress, nDeployGas, poEngineInitData.Web3HttpUrl);
-				}
+                    EngineProps.RulesEngine =
+                        new WonkaBizRulesEngine(new StringBuilder(EngineProps.RulesMarkupXml), EngineProps.SourceMap, EngineProps.MetadataSource);
 
-				if (String.IsNullOrEmpty(poEngineInitData.RegistryContractAddress))
-				{
-					var RegistryDeployment = new Autogen.WonkaRegistry.WonkaRegistryDeployment();
+                    EngineProps.RulesEngine.DefaultSource = poEngineInitData.StorageDefaultSourceId;
 
-					HexBigInteger nDeployGas = new HexBigInteger(CONST_DEPLOY_DEFAULT_CONTRACT_GAS_COST);
-
-					poEngineInitData.RegistryContractAddress =
-						RegistryDeployment.DeployContract(web3, poEngineInitData.RegistryContractABI, poEngineInitData.EthSenderAddress, nDeployGas, poEngineInitData.Web3HttpUrl);
-				}
-
-				if (String.IsNullOrEmpty(poEngineInitData.StorageContractAddress))
-				{
-					var TestContractDeployment = new Autogen.WonkaTestContract.WonkaTestContractDeployment();
-
-					HexBigInteger nDeployGas = new HexBigInteger(CONST_DEPLOY_DEFAULT_CONTRACT_GAS_COST);
-
-					poEngineInitData.StorageContractAddress =
-						TestContractDeployment.DeployContract(web3, poEngineInitData.StorageContractABI, poEngineInitData.EthSenderAddress, nDeployGas, poEngineInitData.Web3HttpUrl);
-				}
-
-				if ((EngineProps.SourceMap == null) || (EngineProps.SourceMap.Count == 0))
-				{
-					EngineProps.SourceMap = new Dictionary<string, WonkaBizSource>();
-
-					// Here a mapping is created, where each Attribute points to a specific contract and its "accessor" methods
-					// - the class that contains this information (contract, accessors, etc.) is of the WonkaBreSource type
-					foreach (WonkaRefAttr TempAttr in WonkaRefEnv.AttrCache)
-					{
-						WonkaBizSource TempSource =
-							new WonkaBizSource(poEngineInitData.StorageDefaultSourceId,
-											   poEngineInitData.EthSenderAddress, 
-											   poEngineInitData.EthPassword, 
-											   poEngineInitData.StorageContractAddress, 
-											   poEngineInitData.StorageContractABI,
-											   poEngineInitData.StorageGetterMethod, 
-											   poEngineInitData.StorageSetterMethod,
-											   EngineProps.DotNetRetrieveMethod);
-
-						EngineProps.SourceMap[TempAttr.AttrName] = TempSource;
-					}
-				}
-
-				EngineProps.RulesEngine = 
-					new WonkaBizRulesEngine(new StringBuilder(EngineProps.RulesMarkupXml), EngineProps.SourceMap, EngineProps.MetadataSource);
-
-				EngineProps.RulesEngine.DefaultSource = poEngineInitData.StorageDefaultSourceId;
-
-				EngineProps.RulesEngine.SetDefaultStdOps(poEngineInitData.EthPassword, poEngineInitData.Web3HttpUrl);
+                    EngineProps.RulesEngine.SetDefaultStdOps(poEngineInitData.EthPassword, poEngineInitData.Web3HttpUrl);
+                }
+                else
+                    throw new Exception("ERROR!  Deployment of Wonka contracts has failed!");
 			}
 		}
 
@@ -240,73 +269,42 @@ namespace Wonka.Eth.Extensions
                 // Using the metadata source, we create an instance of a defined data domain
                 WonkaRefEnvironment WonkaRefEnv = WonkaRefEnvironment.CreateInstance(false, EngineProps.MetadataSource);
 
-                var account = new Account(poEngineInitData.EthPassword);
+                bool bDeploySuccess = await poEngineInitData.DeployContractsClassicAsync().ConfigureAwait(false);
 
-                Nethereum.Web3.Web3 web3 = null;
-                if (!String.IsNullOrEmpty(poEngineInitData.Web3HttpUrl))
-                    web3 = new Nethereum.Web3.Web3(account, poEngineInitData.Web3HttpUrl);
-                else
-                    web3 = new Nethereum.Web3.Web3(account);
-
-                if (String.IsNullOrEmpty(poEngineInitData.RulesEngineContractAddress))
+                if (bDeploySuccess)
                 {
-                    var EngineDeployment = new Autogen.WonkaEngine.WonkaEngineDeployment();
-
-                    HexBigInteger nDeployGas = new HexBigInteger(CONST_DEPLOY_ENGINE_CONTRACT_GAS_COST);
-
-                    poEngineInitData.RulesEngineContractAddress =
-                        await EngineDeployment.DeployContractAsync(web3, poEngineInitData.RulesEngineABI, poEngineInitData.EthSenderAddress, nDeployGas, poEngineInitData.Web3HttpUrl).ConfigureAwait(false);
-                }
-
-                if (String.IsNullOrEmpty(poEngineInitData.RegistryContractAddress))
-                {
-                    var RegistryDeployment = new Autogen.WonkaRegistry.WonkaRegistryDeployment();
-
-                    HexBigInteger nDeployGas = new HexBigInteger(CONST_DEPLOY_DEFAULT_CONTRACT_GAS_COST);
-
-                    poEngineInitData.RegistryContractAddress =
-                        await RegistryDeployment.DeployContractAsync(web3, poEngineInitData.RegistryContractABI, poEngineInitData.EthSenderAddress, nDeployGas, poEngineInitData.Web3HttpUrl).ConfigureAwait(false);
-                }
-
-                if (String.IsNullOrEmpty(poEngineInitData.StorageContractAddress))
-                {
-                    var TestContractDeployment = new Autogen.WonkaTestContract.WonkaTestContractDeployment();
-
-                    HexBigInteger nDeployGas = new HexBigInteger(CONST_DEPLOY_DEFAULT_CONTRACT_GAS_COST);
-
-                    poEngineInitData.StorageContractAddress =
-                        await TestContractDeployment.DeployContractAsync(web3, poEngineInitData.StorageContractABI, poEngineInitData.EthSenderAddress, nDeployGas, poEngineInitData.Web3HttpUrl).ConfigureAwait(false);
-                }
-
-                if ((EngineProps.SourceMap == null) || (EngineProps.SourceMap.Count == 0))
-                {
-                    EngineProps.SourceMap = new Dictionary<string, WonkaBizSource>();
-
-                    // Here a mapping is created, where each Attribute points to a specific contract and its "accessor" methods
-                    // - the class that contains this information (contract, accessors, etc.) is of the WonkaBreSource type
-                    foreach (WonkaRefAttr TempAttr in WonkaRefEnv.AttrCache)
+                    if ((EngineProps.SourceMap == null) || (EngineProps.SourceMap.Count == 0))
                     {
-                        WonkaBizSource TempSource =
-                            new WonkaBizSource(poEngineInitData.StorageDefaultSourceId,
-                                               poEngineInitData.EthSenderAddress,
-                                               poEngineInitData.EthPassword,
-                                               poEngineInitData.StorageContractAddress,
-                                               poEngineInitData.StorageContractABI,
-                                               poEngineInitData.StorageGetterMethod,
-                                               poEngineInitData.StorageSetterMethod,
-                                               EngineProps.DotNetRetrieveMethod);
+                        EngineProps.SourceMap = new Dictionary<string, WonkaBizSource>();
 
-                        EngineProps.SourceMap[TempAttr.AttrName] = TempSource;
+                        // Here a mapping is created, where each Attribute points to a specific contract and its "accessor" methods
+                        // - the class that contains this information (contract, accessors, etc.) is of the WonkaBreSource type
+                        foreach (WonkaRefAttr TempAttr in WonkaRefEnv.AttrCache)
+                        {
+                            WonkaBizSource TempSource =
+                                new WonkaBizSource(poEngineInitData.StorageDefaultSourceId,
+                                                   poEngineInitData.EthSenderAddress,
+                                                   poEngineInitData.EthPassword,
+                                                   poEngineInitData.StorageContractAddress,
+                                                   poEngineInitData.StorageContractABI,
+                                                   poEngineInitData.StorageGetterMethod,
+                                                   poEngineInitData.StorageSetterMethod,
+                                                   EngineProps.DotNetRetrieveMethod);
+
+                            EngineProps.SourceMap[TempAttr.AttrName] = TempSource;
+                        }
                     }
+
+                    EngineProps.RulesEngine =
+                        new WonkaBizRulesEngine(new StringBuilder(EngineProps.RulesMarkupXml), EngineProps.SourceMap, EngineProps.MetadataSource);
+
+                    EngineProps.RulesEngine.DefaultSource = poEngineInitData.StorageDefaultSourceId;
+
+                    //NOTE: These Ethereum ops will not currently execute correctly within .NET during Async mode, since the Wonka.NET must then also be refactored to execute in Async
+                    //EngineProps.RulesEngine.SetDefaultStdOps(poEngineInitData.EthPassword, poEngineInitData.Web3HttpUrl);
                 }
-
-                EngineProps.RulesEngine =
-                    new WonkaBizRulesEngine(new StringBuilder(EngineProps.RulesMarkupXml), EngineProps.SourceMap, EngineProps.MetadataSource);
-
-                EngineProps.RulesEngine.DefaultSource = poEngineInitData.StorageDefaultSourceId;
-
-                //NOTE: These Ethereum ops will not currently execute correctly within .NET during Async mode, since the Wonka.NET must then also be refactored to execute in Async
-                //EngineProps.RulesEngine.SetDefaultStdOps(poEngineInitData.EthPassword, poEngineInitData.Web3HttpUrl);
+                else
+                    throw new Exception("ERROR!  Deployment of Wonka contracts has failed!");
             }
 
             return bResult;
